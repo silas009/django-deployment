c80bb4a8b1e894345ed652d5311f26b8a5814a1b
silas oshiogwemo <silasoshiogwemo@gmail.com>
2025-11-06 18:31:55 +0100

    Add STATIC_ROOT for admin static files

--- learning_users/settings.py
+++ learning_users/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
